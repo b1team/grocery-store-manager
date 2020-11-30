92620418f28bd99123e1a1a789bce4716ebc955f
vuonglv <it.vuonglv@gmail.com>
2020-11-30 08:27:20 +0700

    init quan ly hang form

---
+++ GroceryStoreManager/QuanLyHang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GroceryStoreManager
+{
+    public partial class QuanLyHang : MetroFramework.Forms.MetroForm
+    {
+        public QuanLyHang()
+        {
+            InitializeComponent();
+        }
+    }
+}
