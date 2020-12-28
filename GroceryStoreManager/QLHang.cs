21ce7a04deffb1317ed1ea3cd4583d83f24de151
tungnt <nguyentutung24@gmail.com>
2020-12-28 16:56:50 +0700

    feat/qlhang

--- GroceryStoreManager/QLHang.cs
+++ GroceryStoreManager/QLHang.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
+using GroceryStoreManager.Domains;
 
 namespace GroceryStoreManager
 {
     public partial class QLHang : UserControl
     {
+        private DatabaseContext session;
         private static QLHang _instance;
         public static QLHang Instance
         {
@@ -25,11 +28,40 @@ namespace GroceryStoreManager
         public QLHang()
         {
             InitializeComponent();
+            session = new DatabaseContext();
+        }
+
+        private void DanhSachMatHang()
+        {
+            try
+            {
+                session.DsMatHang.Load();
+                var filter = session.DsMatHang.Local.ToBindingList().OrderByDescending(kinhdoanh => kinhdoanh.DangKinhDoanh);
+                GridQLHang.DataSource = filter.ToList();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Xảy ra lỗi!!!");
+            }
         }
 
         private void QLHang_Load(object sender, EventArgs e)
         {
-            
+            DanhSachMatHang();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                session.SaveChanges();
+                MessageBox.Show("Lưu thành công");
+                DanhSachMatHang();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lưu không thành công");
+            }
         }
     }
 }
